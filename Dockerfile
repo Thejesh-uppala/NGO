3cca2e4845e1baf2a153ef1b8bc169a8bc325fbe
Thejesh-uppala <thejeshmulinja123@gmail.com>
2024-10-30 02:27:24 +0530

    single service docker

--- Dockerfile
+++ Dockerfile
@@ -1,44 +1,27 @@
 # Step 1: Build the Angular application
 FROM --platform=linux/amd64 node:18 AS ng-builder
- 
-# Set the working directory for the Angular build
 WORKDIR /app/NGO.Web/ClientApp
 COPY ./NGO.Web/ClientApp/package*.json ./
 RUN npm install
 COPY ./NGO.Web/ClientApp .
-RUN npm run build 
- 
+RUN npm run build  
+
 # Step 2: Build the .NET application
 FROM --platform=linux/amd64 mcr.microsoft.com/dotnet/sdk:6.0 AS build
- 
-# Set the working directory inside the container
 WORKDIR /app
 COPY . .
- 
-# Copy the solution file and restore dependencies
 COPY *.sln ./
 COPY NGO.Web/*.csproj ./NGO.Web/
 RUN dotnet restore
- 
-# Copy the entire application and build it
 WORKDIR /app/NGO.Web
 RUN dotnet publish -c Release -o out
- 
+
 # Step 3: Create the final image using the ASP.NET Core runtime
 FROM --platform=linux/amd64 mcr.microsoft.com/dotnet/aspnet:6.0 AS runtime
- 
-# Set the working directory inside the container
 WORKDIR /app
- 
-# Copy the published application from the build stage
-COPY --from=build /app/NGO.Web/out .
- 
-# Copy the Angular build output to the wwwroot folder
-COPY --from=ng-builder /app/NGO.Web/ClientApp/dist/ClientApp ./wwwroot
-
-# Expose the port your application runs on
+COPY --from=build /app/NGO.Web/out . 
+# Expose the port
 EXPOSE 80
- 
 
-# Specify the entry point for the container
+# Entry point
 CMD ["dotnet", "NGO.Web.dll"]
--- NGO.Web/Program.cs
+++ NGO.Web/Program.cs
@@ -1,17 +1,17 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NGO.Common;
 using NGO.Web.Infrastructure;
 using System.Text;
-using Microsoft.AspNetCore.Http.Features;
-using NGO.Repository;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.Extensions.FileProviders;
+using NGO.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Bind configurations
 ConfigurationManager ConfigurationManager = builder.Configuration;
 IWebHostEnvironment Environment = builder.Environment;
 var configRoot = new
@@ -20,7 +20,7 @@ var configRoot = new
 };
 ConfigurationManager.Bind(configRoot);
 
-// Add services to the container.
+// Add services to the container
 string connectionString = System.Environment.GetEnvironmentVariable("DATABASE_URL")
                           ?? builder.Configuration["AppSettings:ConnectionString"];
 
@@ -31,21 +31,20 @@ builder.Services.RegisterDependency(configRoot.AppSettings);
 builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: "CorsPolicy",
-                      builder =>
-                      {
-                          builder.AllowAnyOrigin().
-                                              AllowAnyMethod().
-                                              AllowAnyHeader();
-                                        
-                      });
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 });
 
 builder.Services.AddSingleton(provider => new MapperConfiguration(cfg =>
 {
     cfg.AddProfile(new AutoMapperConfig());
 }).CreateMapper());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -54,49 +53,30 @@ builder.Services.AddSwaggerGen(options =>
         Version = "v1",
         Title = "NGO API",
         Description = "An ASP.NET Core Web API for managing ToDo items",
-        TermsOfService = null,
     });
-    options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Description = "JWT Authorization header using the Bearer scheme.",
         Name = "Authorization",
         In = ParameterLocation.Header,
         Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
     });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-      {
-        {
-          new OpenApiSecurityScheme
-          {
-            Reference = new OpenApiReference
-              {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-              },
-              Scheme = "oauth2",
-              Name = "Bearer",
-              In = ParameterLocation.Header,
-
-            },
-            new List<string>()
-          }
-        });
-
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" } }, new List<string>() }
+    });
 });
+
 var key = Encoding.ASCII.GetBytes(configRoot.AppSettings.Secret);
 builder.Services.AddAuthentication(x =>
 {
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, x =>
+}).AddJwtBearer(x =>
 {
     x.RequireHttpsMetadata = false;
     x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters()
+    x.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(key),
@@ -105,37 +85,27 @@ builder.Services.AddAuthentication(x =>
         ClockSkew = TimeSpan.FromDays(1),
     };
 });
-// In production, the Angular files will be served from this directory
-builder.Services.AddSpaStaticFiles(configuration =>
-{
-    configuration.RootPath = "ClientApp/dist/ClientApp";
-});
+
 builder.Services.Configure<FormOptions>(o =>
 {
     o.ValueLengthLimit = int.MaxValue;
     o.MultipartBodyLengthLimit = long.MaxValue;
     o.MemoryBufferThreshold = int.MaxValue;
 });
-//builder.Services.AddControllersWithViews()
-//    .AddNewtonsoftJson(options =>
-//    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-//);
+
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
+// Configure middleware
 if (app.Environment.IsDevelopment())
 {
-    app.UseCors(x => x.AllowAnyMethod().
-                       AllowAnyHeader().
-                       WithHeaders().
-                       AllowCredentials().
-                       SetIsOriginAllowed(origin =>
-                       true));
+    app.UseCors(x => x.AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials()
+                       .SetIsOriginAllowed(origin => true));
 }
+
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
@@ -143,68 +113,17 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "NGO API V1");
-    c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
 });
 
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseSpaStaticFiles();
-//}
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseSpaStaticFiles(new StaticFileOptions
-    {
-        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "ClientApp", "dist", "ClientApp"))
-    });
-}
-
-
-
-#region Custom Middlewares
-app.UseApplicationMiddleware();
-#endregion
+// Serve static files from wwwroot
+app.UseStaticFiles();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller}/{action=Index}/{id?}");
     endpoints.MapControllers();
 });
 
-app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "ClientApp";
-    if (app.Environment.IsDevelopment())
-    {
-        spa.UseAngularCliServer(npmScript: "start");
-    }
-    else
-    {
-        spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
-        {
-            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "ClientApp", "dist", "ClientApp"))
-        };
-    }
-});
+// Fallback to index.html for SPA routing
+app.MapFallbackToFile("/ClientApp/index.html"); // Adjusted path
 
-//app.MapFallbackToFile("index.html"); ;
-//app.UseSpa(spa =>
-//{
-//    spa.Options.SourcePath = "ClientApp";
-
-//    if (app.Environment.IsDevelopment())
-//    {
-//        //spa.UseProxyToSpaDevelopmentServer("https://community-ngo.vercel.app/");
-
-//        // NOTE: Disable above line and enable below line to trigger angular from dev server.
-//        spa.UseAngularCliServer(npmScript: "start");
-//    }
-//     else
-//    {
-//        // In production, ensure the default document is set to index.html
-//        spa.Options.DefaultPage = "/index.html"; // Ensures the app serves the index.html file
-//    }
-//});
 app.Run();
